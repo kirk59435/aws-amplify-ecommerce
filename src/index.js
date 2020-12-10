774d1231f0c38b0a061c17f97bcfb488cf3a02ae
unknown <kirk59435@gmail.com>
2020-12-10 12:22:33 -0500

    fix a bug

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ import App from './App';
 import Product from './pages/Product'
 import Checkout from './pages/Checkout'
 import PlacedOrder from './pages/PlacedOrder'
-import CommunityProject from './pages/CommunityProject'
 
 import awsconfig from './aws-exports'
 Amplify.configure(awsconfig)
